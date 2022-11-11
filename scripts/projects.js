6bb51d6ab9606f7cefa237e30a8ec3e037372e46
alabador <austinlabador@gmail.com>
2022-11-11 13:59:50 -0800

    Add button to view more additional projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,4 @@
-
-const projects = [
+export const projects = [
     {title:'Daily CodeWars', info:'Coding challenges to help practice and improve programming skills.'},
     {title:'Frontend UI', info:'A collection of UI and frontend components.'},
     {title:'Sign Up Form', info:'A responsive sign up form for an arctic expedition.'},
