e12d0723589ebc19805535c07f905a7d448de559
alabador <austinlabador@gmail.com>
2022-11-11 13:25:22 -0800

    Create project for objects

---
+++ scripts/projects.js
@@ -0,0 +1,7 @@
+
+const projects = [
+    {title:'Daily CodeWars', info:'Coding challenges to help practice and improve programming skills.'},
+    {title:'Frontend UI', info:'A collection of UI and frontend components.'},
+    {title:'Sign Up Form', info:'A responsive sign up form for an arctic expedition.'},
+    {title:'Admin Dashboard', info:'An administrative Dashboard built using HTML and SCSS.'},
+]
