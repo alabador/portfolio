3aeb472b3f75fb06b86c9144196d251d48a21295
alabador <austinlabador@gmail.com>
2023-02-22 01:42:59 -0800

    Add mobile menu bar and functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,21 @@ const toggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html')
 const scroll = document.querySelector('#scroll-top')
 const rootElement = document.documentElement;
+const mobileMenu = document.querySelector('#mobile-menu')
+const overlay = document.querySelector('#overlay')
 
 toggle.addEventListener('change', switchTheme);
 scroll.addEventListener('click', scrollToTop);
+
+mobileMenu.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+})
+
+overlay.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        overlay.classList.add('hidden');
+    })
+}) 
 
 function switchTheme(){
     if (toggle.checked) {
