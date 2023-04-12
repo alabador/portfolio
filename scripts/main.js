a49fd6de0dc5da5e2a21bc33fc0ec36e298ff19e
Austin Labador <austinlabador@gmail.com>
2023-04-12 01:12:54 -0700

    Remove dark/light toggle and fix mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,22 @@ const scroll = document.querySelector('#scroll-top')
 const rootElement = document.documentElement;
 const mobileMenu = document.querySelector('#mobile-menu')
 const overlay = document.querySelector('#overlay')
+const header = document.querySelector('header')
 
-toggle.addEventListener('change', switchTheme);
+// toggle.addEventListener('change', switchTheme);
 scroll.addEventListener('click', scrollToTop);
 
 mobileMenu.addEventListener('click', () => {
     overlay.classList.remove('hidden');
+    scroll.classList.add('hidden')
+    document.body.style.overflow = 'hidden'
 })
 
 overlay.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', () => {
         overlay.classList.add('hidden');
+        scroll.classList.remove('hidden')
+        document.body.style.overflow = 'visible'
     })
 }) 
 
@@ -22,14 +27,14 @@ overlay.querySelector('#menu-close').addEventListener('click', () => {
     overlay.classList.add('hidden');
 })
 
-function switchTheme(){
-    if (toggle.checked) {
-        html.classList.remove('dark');
-    }
-    else {
-        html.classList.add('dark');
-    }
-}
+// function switchTheme(){
+//     if (toggle.checked) {
+//         html.classList.remove('dark');
+//     }
+//     else {
+//         html.classList.add('dark');
+//     }
+// }
 
 function scrollToTop() {
     rootElement.scrollTo({
